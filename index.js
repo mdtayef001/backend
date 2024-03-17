78c55582a99d7ee2794a3005b8b78406817223c4
Tayef <tayefhossain200@gmail.com>
2024-03-17 23:28:18 +0600

    test_backend

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require("express");
+require("dotenv").config();
+import express from "express";
 const app = express();
-const port = 4000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -10,6 +10,10 @@ app.get("/tayef", (req, res) => {
   res.send("tayef hossain nabil");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.get("/login", (req, res) => {
+  res.send("<h1>please login</h1>");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`);
 });
