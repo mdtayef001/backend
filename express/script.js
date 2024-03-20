54cd7b5833ad87ac85fec4ca1e25b04693883122
Tayef <tayefhossain200@gmail.com>
2024-03-20 20:45:50 +0600

    express-js-end

--- express/script.js
+++ express/script.js
@@ -2,15 +2,34 @@ const express = require("express");
 const app = express();
 const port = 4000;
 
+app.set("view engine", "ejs");
+app.use(express.static("./public"));
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("index");
 });
 
-app.get("/profile", (req, res) => {
-  res.send("hello from profile");
+app.get("/error", (req, res) => {
+  throw new Error(`${req.ip}:${req.port}`);
 });
-app.get("/profile/:username", (req, res) => {
-  res.send(`hello from profile ${req.params.username}`);
+
+app.get("/contact", (req, res) => {
+  res.render("contact", { name: "tayef hossain nabil" });
+});
+
+// app.get("/profile", (req, res) => {
+//   res.send("hello from profile");
+// });
+// app.get("/profile/:username", (req, res) => {
+//   res.send(`hello from profile ${req.params.username}`);
+// });
+
+app.use(function errorHandler(err, req, res, next) {
+  if (res.headersSent) {
+    return next(err);
+  }
+  res.status(500);
+  res.render("error", { error: err });
 });
 
 app.listen(port, () => {
